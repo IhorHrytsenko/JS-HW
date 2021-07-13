42cb44944b2f0c244283e044759a82ef2bb1657b
IhorHrytsenko <gisyshka@gmail.com>
2021-07-13 12:52:29 +0300

    hw-06-done

--- homework-07/src/js/task-06.js
+++ homework-07/src/js/task-06.js
@@ -1,23 +1,14 @@
 const input = document.querySelector('#validation-input');
 
-const checkLength = () => {
+input.addEventListener('blur', checkLength);
+
+function checkLength () {
+    input.classList.add('invalid');
     if (input.value.length < input.dataset.length){
         input.classList.replace('valid', 'invalid');
-        input.classList.add('invalid');
     } else {
         input.classList.replace('invalid' ,'valid'); 
-        input.blur();
     }
-    console.log(input.value.length);
-    console.log(input.dataset.length);
-
 }
 
-input.addEventListener('keyup', checkLength());
-
-
-
-// console.log(checkLength(input));
-// console.log(input.value.length);
-// console.log(input.dataset.length);
 
