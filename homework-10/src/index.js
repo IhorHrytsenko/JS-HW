e7719240a77b524893b0a23ebcc1b062c4e8830a
IhorHrytsenko <gisyshka@gmail.com>
2021-08-03 16:09:31 +0300

    hw10-template-done

--- homework-10/src/index.js
+++ homework-10/src/index.js
@@ -1,6 +1,20 @@
 import './sass/main.scss';
 import jsonText from './menu.json';
+import templateHTML from '/templates/cardTemplate.hbs'
 
 const menuItem = jsonText;
 
 console.log(menuItem);
+
+
+function createHTML(temps) {
+return templateHTML(temps);
+}
+
+const resultTemplate = createHTML(menuItem);
+
+// console.log(resultTemplate);
+
+const mainList = document.querySelector(".js-menu");
+
+mainList.insertAdjacentHTML("afterbegin", resultTemplate);
